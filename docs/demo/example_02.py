4c41c08aa43a55dec9c9ff04edb898ecb68ca9dc
Francesco De Carlo <decarlof@gmail.com>
2016-12-12 16:47:29 -0600

    moved all python scripts from the library root (stp_core) to docs/demo/

---
+++ docs/demo/example_02.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Example script 
+"""
+
+from __future__ import print_function
+import project
+
+if __name__ == '__main__':
+
+    parameter_01 = 1
+    parameter_02 = 2
+    parameter_03 = 3
+
+    # call function_01
+    result = project.function_02(parameter_01, parameter_02, parameter_03)
+    print (result)
